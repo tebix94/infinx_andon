7c7f89a0f11dcae1da821cb9079ee472ae82ad67
tebix <esteban.torres.perez.1994@gmail.com>
2026-06-09 11:02:47 -0700

    Adding pandas and matplotlib modules. Adding new background task to build a downtime report PNG file by using matplotlib tools. The background task sents the PNG file every hour.

--- app/background_tasks.py
+++ app/background_tasks.py
@@ -1,12 +1,18 @@
 import os
+import tempfile
 from filelock import FileLock, Timeout
 from app.models import Machines, Posts
 from app.telegram_bot import send_telegram_notification 
 from datetime import datetime
 from collections import defaultdict
-from sqlalchemy import or_
+from sqlalchemy import or_, and_
+import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
-def background_task_telegram_short_status_report(scheduler, only_downtime=False):
+def background_task_telegram_status_report(scheduler, only_downtime=False):
     # Cross-platform lock (works in Linux/WSL and Windows)
     lock = FileLock("infinx_telegram.lock")
     
@@ -46,11 +52,11 @@ def background_task_telegram_short_status_report(scheduler, only_downtime=False)
                         continue
 
                     if not active_post:
-                        message += f'<b>{machine.name}</b> corriendo ✅\nHoy ha parado {total_downtime} minutos.\n\n'
+                        message += f'<b>{machine.name}</b> corriendo ✅\nHoy ha parado {total_downtime} Minutes.\n\n'
                     else: 
                         unique_faults = sorted({post.interruption_type.name.lower() for post in machine_posts if post.end_date is None})
                         faults = ", ".join(unique_faults)
-                        message += f'<b>{machine.name}</b> detenido por [{faults}] 🛑\nHoy ha parado {total_downtime} minutos.\n\n'
+                        message += f'<b>{machine.name}</b> detenido por [{faults}] 🛑\nHoy ha parado {total_downtime} Minutes.\n\n'
                 
                 send_telegram_notification(token, chat_id, message)
     except Timeout:
@@ -60,8 +66,159 @@ def background_task_telegram_short_status_report(scheduler, only_downtime=False)
 def run_background_tasks(scheduler):
     scheduler.add_job(
         id='telegram_update_report',
-        func=background_task_telegram_short_status_report,
+        func=background_task_telegram_status_report,
         args=[scheduler, True],
+        trigger='cron',
+        hour='*',
+        minute=0,
+        second=0,
+        replace_existing=True # Prevents duplicate errors on reload
+    )
+
+def background_task_telegram_graph_report(scheduler):
+    # Cross-platform lock (works in Linux/WSL and Windows)
+    lock = FileLock("infinx_telegram_graph.lock")
+
+    try:
+        with lock.acquire(timeout=0):
+            with scheduler.app.app_context():
+                # Fetch environment variables
+                token = os.environ.get('TELEGRAM_BOT_TOKEN')
+                chat_id = os.environ.get('TELEGRAM_INFINX_GROUP_ID')
+                machines = Machines.query.all()
+                machine_downtime = {machine.name: 0 for machine in machines}
+                machine_events = {machine.name: 0 for machine in machines}
+                machine_fault_status = {machine.name: False for machine in machines}
+                current_datetime = datetime.now()
+                shift_string = ''
+
+                if current_datetime.hour < 6:
+                    start_date = current_datetime.replace(hour=0, minute=0, second=0, microsecond=0)
+                    end_date = current_datetime.replace(hour=5, minute=59, second=59, microsecond=0)
+                    shift_string = 'tercer turno'
+                elif current_datetime.hour <= 15 and current_datetime.minute < 30:
+                    start_date = current_datetime.replace(hour=6, minute=0, second=0, microsecond=0)
+                    end_date = current_datetime.replace(hour=15, minute=29, second=59, microsecond=0)
+                    shift_string = 'primer turno'
+                elif current_datetime.hour <= 23:
+                    start_date = current_datetime.replace(hour=15, minute=0, second=0, microsecond=0)
+                    end_date = current_datetime.replace(hour=23, minute=59, second=59, microsecond=0)
+                    shift_string = 'segundo turno'
+
+                # Fetch objects from database
+                posts = Posts.query.filter(or_(Posts.end_date.is_(None), and_(start_date <= Posts.start_date, Posts.start_date <= end_date)))
+
+                for post in posts:
+                    # Calculate downtime delta
+                    if post.end_date == None:
+                        downtime = int((current_datetime - post.start_date).total_seconds() / 60)
+                    else:
+                        downtime = int((post.end_date - post.start_date).total_seconds() / 60)
+
+                    machine_downtime[post.machine.name] = machine_downtime.get(post.machine.name, 0) + downtime
+                    machine_events[post.machine.name] += 1
+                    machine_fault_status[post.machine.name] = True
+
+                report_data = []
+                for m_name in machine_downtime.keys():
+                    report_data.append({
+                        'Machines': m_name,
+                        'Minutes': machine_downtime[m_name],
+                        'Events': machine_events[m_name],
+                        'Status': "● Detenida" if machine_fault_status[m_name] else "● Operando"
+                    })
+
+                # Build Pandas dataframe
+                df = pd.DataFrame(report_data)
+                fig = plt.figure(figsize=(25, 18))
+                gs = gridspec.GridSpec(2, 1, height_ratios=[2.5, 1], hspace=0.6)
+
+                # Define axes as sepparate
+                ax = fig.add_subplot(gs[0])
+                ax_table = fig.add_subplot(gs[1])
+
+                # Build bar plot
+                bar_labels = df['Machines']
+                bar_data = df['Minutes']
+
+                bar_graph = ax.bar(bar_labels, bar_data, color="#0d30ae", edgecolor='white', linewidth=1.2)
+                ax.set_title(f'Tiempos de paro por equipo - {shift_string}', style='italic', fontweight='bold', fontsize=25)
+                ax.set_ylabel('Minutos', fontsize=25)
+                ax.tick_params(axis='x', rotation=45, labelsize=12)
+                ax.grid(axis='x', linestyle='--', alpha=0.7)
+                ax.grid(axis='y', linestyle='--', alpha=0.7)
+
+                for rect in bar_graph:
+                    height = rect.get_height()
+                    if height != 0:
+                        ax.text(
+                            rect.get_x() + rect.get_width() / 2, # Posición X (centro de la barra)
+                            height,                              # Posición Y (altura de la barra)
+                            f'{int(height)}',                    # Texto (valor)
+                            ha='center',                         # Alineación horizontal
+                            va='bottom',                         # Alineación vertical
+                            fontsize=14,                         # Tamaño de letra
+                            fontweight='bold',
+                            color='black'
+                        )
+
+                # Build table
+                df_filtered = df[df['Minutes'] != 0]
+
+                if not df_filtered.empty:
+                    table_data = [[row['Minutes'], row['Events'], row['Status']] for _, row in df_filtered.iterrows()]
+
+                    table = ax_table.table(cellText=table_data,
+                            rowLabels=df_filtered['Machines'].tolist(),
+                            colLabels=['Minutes', 'Incidencias', 'Status'],
+                            loc='center', cellLoc='center',
+                            )
+                    ax_table.axis('off')
+
+                    # Customize the table style
+                    table.auto_set_font_size(False)
+                    table.set_fontsize(15)
+                    table.scale(1, 2.5)
+
+                    # Style the headers
+                    for (row, col), cell in table.get_celld().items():
+                        if row == 0:  # Header row
+                            cell.set_text_props(weight='bold', color='white', fontsize=16)
+                            cell.set_facecolor('#40466e')
+                        else:  # Data rows
+                            cell.set_facecolor('#f8f9fa')
+                            cell.set_text_props(weight='bold', color='black', fontsize=14)
+                        if row > 0 and col == 2:
+                            val = cell.get_text().get_text()
+                            if 'Detenida' in val: cell.get_text().set_color('#ff4d4d')
+                            elif 'Operando' in val: cell.get_text().set_color('#2ecc71')
+                            
+                        # Optional: Add borders to all cells
+                        cell.set_edgecolor('#dcdcdc')
+                else: # When table is empty
+                    ax_table.text(0.5, 0.5, 'No hay paros activos en este turno', 
+                                ha='center', va='center', fontsize=20, fontweight='bold')
+                    ax_table.axis('off')
+
+                # Set file path to OS temp diretory
+                temp_file = tempfile.NamedTemporaryFile(suffix='.png', prefix='andon_report_', delete=False)
+                temp_path = temp_file.name
+                temp_file.close()
+
+                plt.subplots_adjust(left=0.3, bottom=0.28, right=0.8, top=0.72)
+                plt.savefig(temp_path, dpi=150, bbox_inches='tight') # DPI 150 da mejor resolución
+                plt.close()
+                print(f'plot has been created at {temp_path}')
+
+                send_telegram_notification(token, chat_id, 'Gráficas de tiempos de paro', temp_path)
+    except Timeout:
+        return
+    
+def run_background_tasks(scheduler):
+    scheduler.add_job(
+        id='telegram_update_graph_report',
+        func=background_task_telegram_graph_report,
+        args=[scheduler,],
         trigger='cron',
         hour='*',
         minute=0,
--- app/telegram_bot.py
+++ app/telegram_bot.py
@@ -1,18 +1,45 @@
 import requests
+import os
 
-def send_telegram_notification(token, chat_id, message):
+def send_telegram_notification(token, chat_id, message, image_path):
 
-    url = f'https://api.telegram.org/bot{token}/sendMessage'
-    try:
-        response = requests.post(url, json={
-            'chat_id': chat_id,
-            'text': message,
-            'parse_mode': 'HTML'
-        }, timeout=5)
-        
-        # Log if the API returns an error (e.g., bot blocked, chat not found)
-        if not response.ok:
-            print(f'Telegram API Error: {response.status_code} - {response.text}')
+    if image_path:
+        url = f'https://api.telegram.org/bot{token}/sendPhoto'
+        data = {'chat_id': chat_id, 'caption': message, 'parse_mode': 'HTML'}
+    else:
+        url = f'https://api.telegram.org/bot{token}/sendMessage'
+        data = {'chat_id': chat_id, 'text': message, 'parse_mode': 'HTML'}
+
+    if image_path:
+        try:
+            # Open the image file in binary mode
+            with open(image_path, 'rb') as image_file:
+                files = {
+                    'photo': image_file
+                }
             
-    except Exception as e:
-        print(f'Failed to send Telegram message: {e}')
+                # Send the request
+                response = requests.post(url, data=data, files=files, timeout=5)
+                
+                # Log if the API returns an error (e.g., bot blocked, chat not found)
+                if not response.ok:
+                    print(f'Telegram API Error: {response.status_code} - {response.text}')
+                    
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        
+        finally:
+            # This will run regardless of whether the request succeeded or failed
+            if os.path.exists(image_path):
+                os.remove(image_path)
+                print("Temporary image file deleted.")
+    else:
+        try:
+         # Send the request
+            response = requests.post(url, data=data, timeout=5)
+            
+            # Log if the API returns an error (e.g., bot blocked, chat not found)
+            if not response.ok:
+                print(f'Telegram API Error: {response.status_code} - {response.text}')
+        except Exception as e:
+            print(f"An error occurred: {e}")
